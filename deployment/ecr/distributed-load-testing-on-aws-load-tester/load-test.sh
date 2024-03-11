e87fbfc4c8075817433d7384a931a1fb95545c37
Bogdan Katishev <bogdan_katishev@hotmail.com>
2024-03-11 14:04:19 +0100

    Add -maxdepth 2 to find to fetch correct main script

--- deployment/ecr/distributed-load-testing-on-aws-load-tester/load-test.sh
+++ deployment/ecr/distributed-load-testing-on-aws-load-tester/load-test.sh
@@ -51,7 +51,7 @@ if [ "$TEST_TYPE" != "simple" ]; then
     aws s3 cp s3://$S3_BUCKET/public/test-scenarios/$TEST_TYPE/$TEST_ID.zip ./ --region $MAIN_STACK_REGION
     unzip $TEST_ID.zip
     # only looks for the first test script file.
-    TEST_SCRIPT=`find . -name "*.${EXT}" | head -n 1`
+    TEST_SCRIPT=`find . -maxdepth 2 -name "*.${EXT}" | head -n 1`
     if [ -z "$TEST_SCRIPT" ]; then
       echo "There is no test script (.${EXT}) in the zip file."
       exit 1
